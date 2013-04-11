035617187da13220775164821677bc897c37cb7e
Brandon Tilley <brandon@brandontilley.com>
2013-04-11 09:57:10 -0700

    Add ResqueWorker export to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('coffee-script');
 module.exports = {
   ResqueEnvironment: require('./lib/resque_environment'),
-  ResqueQueue: require('./lib/resque_queue')
+  ResqueQueue: require('./lib/resque_queue'),
+  ResqueWorker: require('./lib/resque_worker')
 };
